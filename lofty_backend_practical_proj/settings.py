a40736f63114a3ac771cbd07bae5af1d6be83a7d
amckee250 <54288107+amckee250@users.noreply.github.com>
2022-12-29 23:09:53 -0500

    Added Dockerfile and compose, swagger docs

--- lofty_backend_practical_proj/settings.py
+++ lofty_backend_practical_proj/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env("SETTINGS_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# NOTE: THIS IS FOR DEMO ONLY, DO NOT ALLOW ALL IN PRODUCTION
+ALLOWED_HOSTS = ['*']
 
 # IPS used for internal testing
 INTERNAL_IPS = [
@@ -91,11 +92,14 @@ WSGI_APPLICATION = 'lofty_backend_practical_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
